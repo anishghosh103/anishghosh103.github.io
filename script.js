a826cc6d3e99eaaf60c3d959435d5d234db8e0d5
Anish Ghosh <anishghosh103@gmail.com>
2018-04-22 19:54:16 +0530

    responsive-navbar-update

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
 
-let btnMenu = document.getElementById('btn-menu');
-let navbar = document.getElementsByClassName('navbar');
-let navItems = document.getElementsByClassName('nav-item');
+let navItems = document.querySelectorAll('.nav-item');
+let cbMenu = document.querySelector("#cb-menu");
 
-btnMenu.addEventListener('click', elem => {
-	if(getComputedStyle(navbar[0]).display === "none") {
-		navbar[0].style.display = "flex";
-	} else {
-		navbar[0].style.display = "none";
-	}
-});
-
-window.addEventListener('resize', event => {
-	if (window.innerWidth <= 600) {
-		btnMenu.style.display = "block";
-		navbar[0].style.display = "none";
-	} else {
-		btnMenu.style.display = "none";
-		navbar[0].style.display = "flex";
-	}
-});
-
-(function () {
-	for (let item of navItems) {
-		item.addEventListener('click', (elem) => {
-			if (window.innerWidth <= 600) {
-				navbar[0].style.display = "none";
-			}
-		});
-	}
-})();
-
-if (window.innerWidth <= 600) {
-	btnMenu.style.display = "block";
+for (let item of navItems) {
+	item.addEventListener("click", e => {
+		cbMenu.checked = !cbMenu.checked;
+	});
 }
